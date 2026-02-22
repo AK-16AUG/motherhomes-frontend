94ab8d81721f6c50ad02d70b8c7dd98c11cb0575
keshav <keshavbruh@gmail.com>
2026-02-22 23:39:01 +0530

    chore: finalize auth fixes and documentation

--- src/utils/Axios/Axios.ts
+++ src/utils/Axios/Axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL:import.meta.env.VITE_BACKEND_URL, // Change this to your API base URL
+  baseURL: import.meta.env.VITE_BACKEND_URL, // Change this to your API base URL
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
@@ -11,7 +11,13 @@ const instance = axios.create({
 
 instance.interceptors.request.use(
   (config: any) => {
-    const token = localStorage.getItem("token");
+    let token = localStorage.getItem("token");
+
+    // Sanitize token: remove any surrounding quotes that might have been added
+    if (token && (token.startsWith('"') && token.endsWith('"'))) {
+      token = token.slice(1, -1);
+    }
+
     if (token && config.headers) {
       config.headers.Authorization = `Bearer ${token}`;
     }
